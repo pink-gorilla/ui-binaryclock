f25c58b4899587cc40b9bd7bb1acb23f71231263
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-07-05 22:23:49 -0500

    edn config - pinkie

--- src/ui/binaryclock/goldly.cljs
+++
@@ -1,6 +0,0 @@
-(ns ui.binaryclock.goldly
-  (:require
-   [pinkie.pinkie :refer-macros [register-component]]
-   [ui.binaryclock.clock :refer [binary-clock]]))
-
-(register-component :p/clock binary-clock)
